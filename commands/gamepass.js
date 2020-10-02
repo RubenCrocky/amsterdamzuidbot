f14a0891fdb105d1ea8e61f4d8bd4856250a092d
RubenCrocky <63790523+RubenCrocky@users.noreply.github.com>
2020-10-02 17:42:55 +0200

    init
    init

--- commands/gamepass.js
+++ commands/gamepass.js
@@ -2,12 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
      
-  
+    var Boombox = 'https://www.roblox.com/game-pass/12042513/Boombox'
+
     var botEmbed = new discord.MessageEmbed()
         .setTitle('Gamepass Amsterdam Zuid.')
         .setDescription("Let erop! Wij betalen geen robux terug!")
         .setColor("#0099ff")
-        .setURL('https://www.roblox.com/game-pass/12042513/Boombox') 
+        .addField("Boombox", `${Boombox}`)
         .setThumbnail('')
         .setTimestamp()
         .setFooter('The bot is maded by: ItzMeRubenツ#5865', '');

dfb75476cc4a1c55825b5386b0dfaef82210e1b5
RubenCrocky <63790523+RubenCrocky@users.noreply.github.com>
2020-10-03 12:52:55 +0200

    init
    init

---
+++ commands/help.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+     
+    try {
+        
+        var helpEmbed = new discord.MessageEmbed()
+        .setTitle("Commands Amsterdam Zuid V1")
+        .setColor("#03befc")
+        .setDescription("Let erop dat je niet gaat spammen met commands.")
+        .addFields(
+            {name:"Algemene Commands", value:"Deze Commands kan iedereen uitvoeren!"},
+            {name:"!avatar", value:"Krijg hiermee je avatar te zien."},
+            {name:"!bug", value:"Zend hiermee een bug naar de developers"},
+            {name:"!suggestie", value:"Zend hiermee een suggestie naar de developers"},
+            {name:"!faq", value:"Krijg de Veelgestelde vragen in een mooi berichtje"},
+            {name:"!help", value:"Krijg dit te zien"},
+            {name:"!Permissie Commands", value:"Deze commands kan je enkel uitvoeren als je bepaalde permissies heb!"},
+            {name:"!announcement", value:"Zend hiermee een announcement naar de discord server!"},
+            {name:"!claim", value:"Claim een ticket"},
+            {name:"!clear", value:"Verwijder berichten"},
+            {name:"!bug", value:"Zend hiermee een bug naar de developers"},
+            {name:"LET EROP", value:"ER word nog hard gewerkt aan commands."}
+    
+        )
+     
+       
+        message.author.send(helpEmbed);
+    
+        message.channel.send("Alle commando's staan in je privé berichten!");
+    
+    } catch (error) {
+        message.reply("Er is iets fout gebeurd");
+    }
+    
+};
+
+module.exports.help = {
+    name: "help"
+}
